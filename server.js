cb30b8f4f0e33c5389317e42d743c54d4bba6d8b
vladdnc <vladdnc@mail.ru>
2017-05-24 13:35:54 +0300

    Prep repo to heroku

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+  if(req.headers['x-forward-proto'] === 'http'){
+    next();
+  }else {
+    res.redirect('https://' + req.url);
+  }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('Server is up on port 3000');
+app.listen(PORT, function() {
+  console.log('Server is up on port ' + PORT);
 });
